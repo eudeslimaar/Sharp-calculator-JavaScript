abd5b986f7d53fe758379812d7ec195182ff6f37
eudeslimaar <eudesfilho1994@outlook.com>
2022-02-17 20:59:14 -0300

    Calculator-version 1

--- index.js
+++ index.js
@@ -1,1 +1,147 @@
 
+
+document.addEventListener("DOMContentLoaded", function(){
+    sharpCalculator = calculatorFactory("#sharp-calculator");
+    sharpCalculator.init();
+});
+function calculatorFactory(cssId) {
+    return {
+ 
+      buttonsList: [],
+      calcMemory: 0,
+      calcValue: 0,
+      screen: 0,
+      screenElement: {},
+     
+      init() {
+        let _this = this;
+
+        this.buttonsList = document.querySelectorAll(`${cssId} .calc-btn`);
+        this.screenElement = document.querySelector(`${cssId} output`);
+      
+       
+        for (var button of this.buttonsList) {
+          button.addEventListener('click', function() {
+            _this.calcWrite(this.innerHTML, _this.screenElement);
+          });
+        }
+      
+        
+      },
+
+      calcWrite(value, affectedScreen) {
+        if (value === "on/c") {
+         
+          affectedScreen.innerText = "0";
+          calcValue = 0;
+        } else if (value === "CE") {
+         
+          var reduceOfOne = affectedScreen.innerText.slice(0, -1);
+          affectedScreen.innerText = reduceOfOne;
+          if (affectedScreen.innerText === "") {
+            affectedScreen.innerText = "0";
+          }
+        } else if (value === "=" || value === "Enter") {
+         
+          affectedScreen.innerText = eval(this.calcValue);
+        } else if (!isNaN(value)) {
+          
+          if (Number(affectedScreen.innerText) === 0) {
+            affectedScreen.innerText = value;
+            this.calcValue = value;
+          } else {
+            affectedScreen.innerText += value;
+            this.calcValue += value;
+          }
+        } else {
+         
+          if (value === "MC" || value === "M+" || value === "M-" || value === "MR") {
+            this.useMemory(value, affectedScreen);
+          } else if (Number(affectedScreen.innerText) === 0) {
+            
+            switch (value) {
+              case "-":
+                affectedScreen.innerText += value;
+                this.calcValue += "-";
+                break;
+              case ".":
+                affectedScreen.innerText = value;
+                this.calcValue = ".";
+                break;
+            }
+          } else {
+            this.setOperator(value, affectedScreen);
+          }
+        }
+      },
+      
+      setOperator(value, affectedScreen) {
+        let regex = /(([^\.]\d{1,}\.\d{1,}$)|(^[\.]\d{1,}))/g;
+        
+        let lastEntry = affectedScreen.innerHTML.slice(-1);
+        let canAddOperator = lastEntry === "+" ? false :
+                             lastEntry === "-" ? false :
+                             lastEntry === "×" || lastEntry === "*" ? false :
+                             lastEntry === "÷" || lastEntry === "/" ? false :
+                             lastEntry === "." || value === "." && affectedScreen.innerHTML.match(regex) ? false :
+                             true;
+        if (canAddOperator) {
+          
+          switch (value) {
+            case "+":
+              affectedScreen.innerText += value;
+              this.calcValue += "+";
+              break;
+            case "-":
+              affectedScreen.innerText += value;
+              this.calcValue += "-";
+              break;
+            case "x"||"*":
+              affectedScreen.innerText += value;
+              this.calcValue += "*";
+              break;
+            case "÷"||"/":
+              affectedScreen.innerText += value;
+              this.calcValue += "/";
+              break;
+            case ".":
+              affectedScreen.innerText += value;
+              this.calcValue += ".";
+              break;
+          }
+        }
+      },
+     
+      useMemory(value, affectedScreen) {
+        
+        switch (value) {
+          case "MC":
+            this.calcMemory = 0;
+            break;
+          case "M+":
+            if(this.isValidMemoryUsage(affectedScreen.innerText)) {
+              this.calcMemory += affectedScreen.innerText * 1;
+              affectedScreen.innerText = 0;
+            } else {
+              console.warn("You cannot memorize a calculation that has not been completed.");
+            }
+            break;
+          case "M-":
+            if(this.isValidMemoryUsage(affectedScreen.innerText)) {
+              this.calcMemory -= affectedScreen.innerText * 1;
+              affectedScreen.innerText = 0;
+            } else {
+              console.warn("You cannot memorize a calculation that has not been completed.");
+            }
+            break;
+          case "MR":
+            affectedScreen.innerText = this.calcMemory;
+            break;
+        }
+      },
+      isValidMemoryUsage(toValidate) {
+        return !isNaN(toValidate);
+      }
+    };
+  }
+      
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 # Calculator - Javascript
+Autor: Eudes Lima.
+Email: developer@eudeslima.com
+Site: Eudeslima.com
+
+
+
+Com esse projeto, fiz uma calculadora construida sem imagens, utilizei somente  CSS puro e dei vida ao projeto com javascript.
+
+## Introducao
+
+### Calculator Sharp Elsi Mate El - 230
+
+Essa é uma calculador de 1982, Calculadora aritmética de la empresa japonesa Sharp, con precisión de 8 dígitos, con lógica algebraica y 6 funciones. Dispone de una pequeña pantalla LCD de 8 dígitos y un teclado de 25 teclas en tres colores: de color negro para los números y las funciones (suma, resta, multiplicación, división, raíz cuadrada y cálculo de porcentajes), las azules para la memoria y la roja para encendido y borrado (el apagado es automático). Incorpora un circuito integrado especifico de Sharp, el LI3324AM, del que no se tienen datos de sus características. 
+
+
+## Consideracoes 
+"Esta é uma pequena homenagem que faco ao meu querido pai, Eudes lima("1949-2021").
